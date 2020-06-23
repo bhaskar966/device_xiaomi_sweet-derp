ce55d8eb74cae9318dde95612dbdd850db52e8f9
dianlujitao <dianlujitao@lineageos.org>
2020-06-23 12:04:45 +0800

    sweet: Remove keymap of headset buttons
    Signed-off-by: bhaskar966 <deybhaskar999@gmail.com>

--- device.mk
+++ device.mk
@@ -345,7 +345,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
-    
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
