7886f65fd314a3bddbc7ef0e4c26ccf41a185c55
bhaskar966 <deybhaskar999@gmail.com>
2023-10-15 23:23:36 +0530

    sweet: vendorsetup.sh: switch to sleepy kernel and r487747c clang
    Signed-off-by: bhaskar966 <deybhaskar999@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -16,5 +16,5 @@ git clone https://github.com/bhaskar966/vendor_xiaomi_sweet.git -b 13.0 vendor/x
 git clone https://github.com/PixelExperience/packages_resources_devicesettings packages/resources/devicesettings
 
 # Kernel
-git clone -b 17 --depth=1 https://gitlab.com/PixelOS-Devices/playgroundtc.git prebuilts/clang/host/linux-x86/clang-playground
-git clone -b courbet-13 --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet
+git clone -b 13.0 --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r487747c.git prebuilts/clang/host/linux-x86/clang-487747c
+git clone -b sleepy --depth=1 https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet
