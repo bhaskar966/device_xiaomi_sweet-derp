1d3a8a8a6b01589ae55c38e8b7810a87ea8e70cc
bhaskar966 <deybhaskar999@gmail.com>
2023-10-17 23:43:24 +0530

    sweet: vendorsetup.sh: track -b 14 vendor
    * alo fix kernel config

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,7 +10,7 @@ git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-13.0-c
 git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/display
 
 # Vendor
-git clone https://github.com/bhaskar966/vendor_xiaomi_sweet.git -b 13.0 vendor/xiaomi/sweet
+git clone https://github.com/bhaskar966/vendor_xiaomi_sweet.git -b 14 vendor/xiaomi/sweet
 
 # Device Settings
 git clone https://github.com/PixelExperience/packages_resources_devicesettings packages/resources/devicesettings
