32cc468a1ead07ddff50fedb08434757cfa5d2f2
bhaskar966 <deybhaskar999@gmail.com>
2023-10-25 23:20:29 +0530

    sweet: vendorsetup.sh: track derp hals
    Now no need to track hals from other sources
    
    Signed-off-by: bhaskar966 <deybhaskar999@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,19 +1,5 @@
-# Remove
-rm -rf hardware/qcom-caf/sm8150/display
-rm -rf hardware/qcom-caf/sm8150/audio
-rm -rf hardware/qcom-caf/sm8150/media
-rm -rf packages/resources/devicesettings
-
-# Hals
-git clone https://github.com/ArrowOS/android_hardware_qcom_audio -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/audio 
-git clone https://github.com/ArrowOS/android_hardware_qcom_media -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/media 
-git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-13.0-caf-sm8150 hardware/qcom-caf/sm8150/display
-
 # Vendor
 git clone https://github.com/bhaskar966/vendor_xiaomi_sweet.git -b 14 vendor/xiaomi/sweet
-
-# Device Settings
-git clone https://github.com/PixelExperience/packages_resources_devicesettings packages/resources/devicesettings
 
 # Kernel
 git clone -b 13.0 --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r487747c.git prebuilts/clang/host/linux-x86/clang-487747c
